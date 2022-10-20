86b0087a3d19d6e1be1a48729c79d5c5792ac945
jessikim <jessica.kim17@gmail.com>
2022-10-20 11:12:36 -0500

    change david things to jess things

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -6,7 +6,7 @@ const Landing = () => {
       {/* All the <Link> components should live here */}
       <h1>All-The-Things</h1>
       <Link to="/the-manliest-things">Ben's Things</Link><br/>
-      <Link to="/the-well-styled-things">David's Things</Link><br/>
+      <Link to="/the-well-styled-things">Jessica's Things</Link><br/>
       <Link to="/the-silly-things">Hunter's Things</Link><br/>
       <Link to="/the-color-things">Caro's Things</Link><br/>
     </>
